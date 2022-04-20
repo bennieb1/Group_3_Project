2c25f76c72a8a70a74888834693ae455258f191d
Brandon1 <bennieb@sheridancollege.ca>
2022-04-20 17:28:48 -0400

    player changes

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -5,6 +5,8 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
@@ -14,16 +16,8 @@ package ca.sheridancollege.project;
 public abstract class Player {
 
     private String name; //the unique name for this player
-
-    /**
-     * A constructor that allows you to set the player's unique ID
-     *
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name) {
-        this.name = name;
-    }
-
+    private int numOfCards;
+    
     /**
      * @return the player name
      */
@@ -38,7 +32,53 @@ public abstract class Player {
      */
     public void setName(String name) {
         this.name = name;
+
     }
+
+    ArrayList<Card> playerHand;
+    /**
+     * A constructor that allows you to set the player's unique ID
+     *
+     * @param name the unique ID to assign to this player.
+     */
+    public Player(String name) {
+        this.name = name;
+        playerHand = new ArrayList<Card>();
+    }
+
+    public void addCard(Card aCard) {
+
+        playerHand.add(aCard);
+        this.numOfCards++;
+    }
+
+    public int getHandSum() {
+
+        int sum = 0;
+
+        for (Card countSum : playerHand) {
+
+            sum = sum + countSum.getFace();
+
+        }
+        return sum;
+    }
+    
+    public void getHand(boolean hideCards) {
+    
+        System.out.println(this.name + "your current hand.");
+
+        for (int c = 0; c < numOfCards; c++) {
+            if (c == 0 && !hideCards) {
+                System.out.println("[Hidden Card]");
+            }
+            else {
+                System.out.println(playerHand.get(c).toString());
+            }
+        }
+    
+    }
+    
 
     /**
      * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
